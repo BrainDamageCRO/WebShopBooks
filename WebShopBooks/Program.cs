26a16648828256a34507614389b7e046e7558d41
robertmaksic <robertmaksic@gmail.cm>
2024-03-13 18:40:59 +0100

    Add project files.

---
+++ WebShopBooks/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using WebShopBooks.DataAccess.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// DI -> Ovdje registriramo sve servise
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Home/Privacy/
+// Home/Privacy/5
+// https://localhost:55555/{controller}/{action}/{id}
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
