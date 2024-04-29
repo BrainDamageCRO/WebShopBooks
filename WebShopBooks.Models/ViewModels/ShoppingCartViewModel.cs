44207a852f2f86e1729218f669bdd2cfa254aad5
robertmaksic <robertmaksiczg@gmail.com>
2024-04-29 14:22:22 +0200

    AddOrderTotal ExpandShoppingCartModelWithPriceNotMapped

--- WebShopBooks.Models/ViewModels/ShoppingCartViewModel.cs
+++ WebShopBooks.Models/ViewModels/ShoppingCartViewModel.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using WebShopBooks.Models.Models;
 
 namespace WebShopBooks.Models.ViewModels;
 
 public class ShoppingCartViewModel
 {
+    [ValidateNever]
     public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
     public double OrderTotal { get; set; }
 }
--- WebShopBooks/Areas/Customer/Controllers/ShoppingCartController.cs
+++ WebShopBooks/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using WebShopBooks.DataAccess.Repository.IRepository;
+using WebShopBooks.Models.Models;
 using WebShopBooks.Models.ViewModels;
 
 namespace WebShopBooks.Areas.Customer.Controllers;
@@ -28,6 +29,31 @@ public class ShoppingCartController : Controller
             ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(sp => sp.ApplicationUserId == userId, includeProperties: "Product")
         };
 
+        foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+        {
+            cart.Price = GetPriceBasedOnQuantity(cart);
+            ShoppingCartViewModel.OrderTotal += (cart.Price * cart.Count);
+        }
+
         return View(ShoppingCartViewModel);
+    }
+
+    private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+    {
+        if (shoppingCart.Count <= 50)
+        {
+            return shoppingCart.Product.Price;
+        }
+        else
+        {
+            if (shoppingCart.Count <= 100)
+            {
+                return shoppingCart.Product.Price50;
+            }
+            else
+            {
+                return shoppingCart.Product.Price100;
+            }
+        }
     }
 }
