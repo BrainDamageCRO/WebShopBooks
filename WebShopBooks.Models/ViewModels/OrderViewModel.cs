6562ccb3934cf245cb5afb1f2f80a1e37b7cee66
robertmaksic <robertmaksiczg@gmail.com>
2024-05-06 16:39:50 +0200

    AddOrderController AddOrderViewModel

---
+++ WebShopBooks.Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿using WebShopBooks.Models.Models;
+
+namespace WebShopBooks.Models.ViewModels;
+
+public class OrderViewModel
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetail { get; set; }
+}
---
+++ WebShopBooks/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using WebShopBooks.DataAccess.Repository.IRepository;
+using WebShopBooks.Models.Models;
+
+namespace WebShopBooks.Areas.Admin.Controllers;
+
+public class OrderController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public OrderController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API Calls
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        List<OrderHeader> orderHeaderList = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        return Json(new { data = orderHeaderList });
+    }
+
+    #endregion
+}
